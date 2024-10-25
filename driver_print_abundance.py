fb2e6302103d68175ff63d99551997ccddce46b1
Hongrui Liu <genius.hongrui.liu@gmail.com>
2024-10-25 14:05:55 -0400

    seperate the function file from the driver file

---
+++ driver_print_abundance.py
@@ -0,0 +1,28 @@
+import json
+from biom import load_table
+from biom import Table
+from print_sample_abundance import print_sample_abundance
+
+table = load_table('dataset/test_export.json')
+
+### PLAYING WITH THE TABLE
+# Get table shape
+num_observations, num_samples = table.shape
+print(f"Number of Observations (OTUs): {num_observations}")
+print(f"Number of Samples: {num_samples}")
+
+# Access sample IDs
+sample_ids = table.ids(axis='sample')
+print(f"Sample IDs: {sample_ids[:5]} ...")  # Display first 5 sample IDs
+
+# Access metadata
+# sample_metadata = table.metadata_to_dataframe('sample')
+observation_metadata = table.metadata_to_dataframe('observation')
+print(observation_metadata)
+
+# Get the metadata associated with a specific OTU
+dict(table.metadata('TACGGAAGGTCCGGGCGTTATCCGGATTTATTGGGTTTAAAGGGAGCGCAGGCCGTCCTTTAAGCGTGCTGTGAAATGCCGCGGCTCAACCGTGGCACTGCAGCGCGAACTGGGGGACTTGAGTA', 'observation'))
+
+# Example usage
+sample_id = '10317.000002606'
+print_sample_abundance(table, sample_id)
--- print_sample_abundance.py
+++ print_sample_abundance.py
@@ -2,26 +2,6 @@ import json
 from biom import load_table
 from biom import Table
 
-table = load_table('dataset/test_export.json')
-
-### PLAYING WITH THE TABLE
-# Get table shape
-num_observations, num_samples = table.shape
-print(f"Number of Observations (OTUs): {num_observations}")
-print(f"Number of Samples: {num_samples}")
-
-# Access sample IDs
-sample_ids = table.ids(axis='sample')
-print(f"Sample IDs: {sample_ids[:5]} ...")  # Display first 5 sample IDs
-
-# Access metadata
-# sample_metadata = table.metadata_to_dataframe('sample')
-observation_metadata = table.metadata_to_dataframe('observation')
-print(observation_metadata)
-
-# Get the metadata associated with a specific OTU
-dict(table.metadata('TACGGAAGGTCCGGGCGTTATCCGGATTTATTGGGTTTAAAGGGAGCGCAGGCCGTCCTTTAAGCGTGCTGTGAAATGCCGCGGCTCAACCGTGGCACTGCAGCGCGAACTGGGGGACTTGAGTA', 'observation'))
-
 def print_sample_abundance(table, sample_id, threshold = 0.001, truncate_length = 5):
     """
     Prints the abundance table for a specified sample.
@@ -65,7 +45,3 @@ def print_sample_abundance(table, sample_id, threshold = 0.001, truncate_length 
     for row in abundance_data:
         row_str = '\t'.join([str(item) for item in row])
         print(row_str)
-
-# Example usage
-sample_id = '10317.000002606'
-print_sample_abundance(table, sample_id)
